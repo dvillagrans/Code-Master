3d6a839547feb5c7e8cb37a90ec7379edb19fab9
Diego Villagran <diegovillasal@gmail.com>
2024-12-01 10:36:06 -0600

    fix

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -7,12 +7,13 @@ import react from "@astrojs/react";
 import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
-  output: 'server',
-  adapter: vercel(),
+
   integrations: [
     tailwind({
       applyBaseStyles: false,
     }),
     react(),
   ],
+  output: 'server',
+  adapter: vercel(),
 });
