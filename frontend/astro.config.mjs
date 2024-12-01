1fa6c4fd0b665c62d8b0f8c7ba515f2da320ee03
Diego Villagran <diegovillasal@gmail.com>
2024-12-01 10:17:21 -0600

    fix

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,9 +1,10 @@
-// @ts-check
-import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel/serverless';
-import react from '@astrojs/react';
-import tailwind from '@astrojs/tailwind';
 
+
+import { defineConfig } from 'astro/config';
+import tailwind from "@astrojs/tailwind";
+import react from "@astrojs/react";
+
+import vercel from "@astrojs/vercel/serverless";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
