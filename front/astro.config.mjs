62ea20522d97c285845ad453424ce9dc2b2641d7
Diego Villagran <diegovillasal@gmail.com>
2024-12-07 00:08:00 -0600

    fix

---
+++ front/astro.config.mjs
@@ -0,0 +1,10 @@
+import { defineConfig } from 'astro/config';
+import vercel from '@astrojs/vercel/serverless';
+import tailwind from '@astrojs/tailwind';
+import react from '@astrojs/react';
+
+export default defineConfig({
+    integrations: [tailwind(), react()],
+    output: 'server',
+    adapter: vercel(),
+});
